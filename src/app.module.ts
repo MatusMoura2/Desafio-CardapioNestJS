4a03e551c485e01e5e8aa944fd79b126ca9e0492
MatusMoura2 <matus.moura333x2@gmail.com>
2023-08-29 22:06:54 -0300

    feat: Iniando CRUD para todas as Entidades

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './modules/product/product.module';
+import { MenuModule } from './modules/menu/menu.module';
+import { CategoryModule } from './modules/category/category.module';
 
 @Module({
-  imports: [],
+  imports: [ProductModule, MenuModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
